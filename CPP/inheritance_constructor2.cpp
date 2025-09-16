10914b2af96658b4b1d610b96a32ce8f6bf4cb20
subhrendu1987 <subhrendu.chattopadhyay@thapar.edu>
2025-09-16 08:00:58 +0530

    new file:   inheritance_constructor2.cpp

---
+++ CPP/inheritance_constructor2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+public:
+	int fun() { cout << "Base::fun() called"; }
+	int fun(int i) { cout << "Base::fun(int i) called"; }
+};
+
+class Derived: public Base{
+public:
+	int fun() { cout << "Derived::fun() called"; }
+};
+
+int main(){
+	Derived d;
+	d.fun(5);
+	return 0;
+}
