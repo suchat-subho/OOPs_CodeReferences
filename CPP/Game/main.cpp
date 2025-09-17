cccb81039ea69825b1f0d768b9632c5f29326050
subhrendu1987 <subhrendu.chattopadhyay@thapar.edu>
2025-09-17 11:13:13 +0530

    modified:   main.cpp

--- CPP/Game/main.cpp
+++ CPP/Game/main.cpp
@@ -1,35 +1,83 @@
 #include<iostream>
 #include <string>
 #include <sstream>
+#include <thread>
+#include <chrono>
+#define DELTA 200 /// Time Quantum in ms (e.g. 40ms for 25FPS)
+#define STRIDE 2
 using namespace std;
+void nextFrame(int waitTimems=DELTA){
+	this_thread::sleep_for(chrono::milliseconds(waitTimems));
+	return;
+}
 
-class character{
+void clearScreen(int waitTimems=DELTA){
+	nextFrame(waitTimems);
+	cout << "\033[2J\033[H"; // Clear screen
+}
 
-	int startPos, endPos;
-	int findPoseNext(int currPos){
-		return(currPos);
-	}
+class Dog{
+	private:
+		int startPos, endPos;
+		/*********/
+		int findPoseNext(int currPos,int stride=STRIDE){
+			return((currPos-stride)>0? currPos-stride: currPos);
+		}
+		/*********/
+		string placement(int pos,string look){
+			string padding(pos, ' ');
+			stringstream ss(look);
+			string line, result;
+	    	while (getline(ss, line)) {result += padding + line + "\n";}
+	    	return(result);
+		}
+		/*********/
+		int run1(int pos){
+			string look="   __\no-''|\\____/)=\n \\_/|_)     )\n    \\   __  \\\n    (_\\    (_\\\n";
+			string output= placement(pos,look);
+			cout <<output<< endl;
+			int nextPos=findPoseNext(pos);
+			return(nextPos);
+		}
+		/*********/
+		int run2(int pos){
+			string look="   __\no-''|\\_____/)=\n \\_/|_)     )\n   |   __  |\n   (_|   (_|\n";
+			string output= placement(pos,look);
+			cout <<output<< endl;
+			int nextPos=findPoseNext(pos);
+			return(nextPos);
+		}
+		/*********/
+		int run3(int pos){
+			string look="   __\no-''|\\_____/)=\n \\_/|_)     )\n   /   __  /\n  (_/   (_/\n";
+			string output= placement(pos,look);
+			cout <<output<< endl;
+			int nextPos=findPoseNext(pos);
+			return(nextPos);
+		}
 	public:
 		/*********/
 		bool isDead(){
 			return(false);
 		}
-		/*********/
-		int run3(int pos){
-			string look="    __\no-''|\\_____/)=\n \\_/|_)     )\n   /   __   \\\n  (_/  (_/\n";
-			string padding(pos, ' ');
-			stringstream ss(look);
-			string line, result;
-    		while (getline(ss, line)) {result += padding + line + "\n";}	
-			cout <<result<< endl;
-			int nextPos=findPoseNext(pos);
-			return(nextPos);
+		void running(int startPos,int till){
+			int nextPos=startPos;
+			for(int i=0;i<till;i++){
+				clearScreen();
+				this->run1(nextPos);
+				clearScreen();
+				nextPos=findPoseNext(nextPos);
+				this->run2(nextPos);
+				clearScreen();
+				nextPos=findPoseNext(nextPos);
+				this->run3(nextPos);
+			}
 		}
 };
 
 int main(){
-	character c;
-	c.run3(10);
-
-
+	Dog woof;
+	clearScreen(0);
+	woof.running(100,100);
+	//clearScreen();
 }
